52db57bd89212e2e644be4541c94185d2b36ac0b
boonprakit <boonprakit@proteus-tech.com>
2017-12-22 18:38:25 +0700

    change code style with flask8

--- compose-coding/djangoTutorial/toDo/urls.py
+++ compose-coding/djangoTutorial/toDo/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'toDo'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^clear/$', views.ClearSelectedView.as_view(), name='clear_selected'),
+    url(r'^clear/$',
+        views.ClearSelectedView.as_view(), name='clear_selected'),
     url(r'^(?P<pk>[0-9]+)/curated/$',
-            views.CurateTodo.as_view(), name='curated_todo'),
+        views.CurateTodo.as_view(), name='curated_todo'),
 ]
