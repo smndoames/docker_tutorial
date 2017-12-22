1517162dfdd825ccf69b29bdbea08137f2f04b7f
boonprakit <boonprakit@proteus-tech.com>
2017-12-22 18:36:57 +0700

    can select and dis select todo

--- compose-coding/djangoTutorial/toDo/urls.py
+++ compose-coding/djangoTutorial/toDo/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'toDo'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^clear/$', views.ClearSelectedView.as_view(), name='clear_selected'),
+    url(r'^(?P<pk>[0-9]+)/curated/$',
+            views.CurateTodo.as_view(), name='curated_todo'),
 ]
--- compose-coding/djangoTutorial/toDo/views.py
+++ compose-coding/djangoTutorial/toDo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-from django.views import generic
+from django.views import generic, View
 
 from .forms import TodoForm
 from .models import ToDo
@@ -8,7 +8,7 @@ from .models import ToDo
 
 class IndexView(generic.ListView):
     def get_queryset(self):
-        return ToDo.objects.all()
+        return ToDo.objects.all().order_by('id')
 
     def get(self, request, *args, **kwargs):
         todos = self.get_queryset()
@@ -27,13 +27,23 @@ class IndexView(generic.ListView):
                       {'form': form, 'todos': todos})
 
 
-class CurateTodo():
+class ClearSelectedView(View):
+    def get_queryset(self):
+        return ToDo.objects.all()
+
     def post(self, request, *args, **kwargs):
         todos = self.get_queryset()
-        form = TodoForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.save()
-            return redirect('toDo:index')
-        return render(request, 'toDo/content.html',
-                      {'form': form, 'todos': todos})
+        for todo in todos:
+            todo.is_selected = False
+            todo.save()
+        return redirect('toDo:index')
+
+
+class CurateTodo(generic.DetailView):
+    model = ToDo
+
+    def post(self, request, *args, **kwargs):
+        todo = self.get_object()
+        todo.is_selected = not todo.is_selected
+        todo.save()
+        return redirect('toDo:index')
